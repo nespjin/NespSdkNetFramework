65a49e04037e5565ee2fa937b12068e4dc6a053b
Jinzhaolu <1756404649@qq.com>
2021-02-24 10:30:31 +0800

    Add ObjectUtils

---
+++ Utils/ObjectUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NespSdkNetFramework.Utils
+{
+    public class ObjectUtils
+    {
+        public static bool CopyProperties(object objFrom, object objTo)
+        {
+            try
+            {
+                Type typeFrom = objFrom.GetType();
+                Type typeTo = objTo.GetType();
+
+                foreach (PropertyInfo propertyInfoFrom in typeFrom.GetProperties())
+                {
+                    foreach (PropertyInfo propertyInfoTo in typeTo.GetProperties())
+                    {
+                        if (propertyInfoFrom.Name == propertyInfoTo.Name)
+                        {
+                            propertyInfoTo.SetValue(objTo, propertyInfoFrom.GetValue(objFrom));
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
