42c09c2d292b197c473579e795c7bf39c81c77ed
JinZhaoLu <1756404649@qq.com>
2021-03-25 22:23:39 +0800

    Change: Move DateTimeExtensions namespace from ```NespSdkNetFramwork.Utils``` to ```NespSdkNetFramwork```

---
+++ DateTimeExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//
+// Copyright (C) 2021 NESP Technology Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// If you have any questions or if you find a bug,
+// please contact the author by email or ask for Issues.
+//
+// Author:JinZhaolu <1756404649@qq.com>
+//
+
+/// <team>NESP Technology</team> 
+/// <author><a href="mailto:1756404649@qq.com">JinZhaolu Email:1756404649@qq.com</a></author>
+/// <date>2021/3/25 21:58:40</date>
+namespace NespSdkNetFramework
+{
+    public static class DateTimeExtensions
+    {
+        public static double TotalMilliseconds(this DateTime dateTime)
+        {
+            return TimeSpan.FromTicks(dateTime.Ticks).TotalMilliseconds;
+        }
+    }
+}
